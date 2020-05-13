8452f085ad91a20358ef6398107720735432b2da
kaaarigar <mailid.nishant@gmail.com>
2020-05-13 16:30:02 +0530

    creating server to get back analytics data

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+var bodyParser = require('body-parser')
+var port = 3000
+
+app.listen(port, function () {
+console.log('We are listening on port ' + port)
+})
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('*', function (req, res) {
+res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+app.post('/num', function (req, res) {
+console.log(req.body);
+
+return res.end('done')
+})
